9d731ba9170f31151f938cfb54422e887fe9c96c
Suchitra Naik <susanaik@gmail.com>
2018-05-19 12:32:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -26,8 +26,8 @@ class Student
   def self.drop_table
     sql = <<-SQL
           DROP TABLE IF EXISTS students
-             SQL 
-          DB[:conn].execute(sql)   
+             SQL
+          DB[:conn].execute(sql)
   end
 
 end
