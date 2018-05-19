7a205f9229d4c34310a09ec9b8afa9c1e9bd506c
Suchitra Naik <susanaik@gmail.com>
2018-05-19 12:48:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,9 +7,9 @@ class Student
   attr_reader :id
 
   def initialize(name, grade, id = nil)
-    @id = id
     @name = name
     @grade = grade
+    @id = id
   end
 
   def self.create_table
@@ -33,7 +33,7 @@ class Student
   def save
     sql = <<-SQL
           INSERT INTO students (name, grade)
-          VALUES(?, ?)
+          VALUES (?, ?)
              SQL
    DB[:conn].execute(sql, self.name, self.grade)
   end
